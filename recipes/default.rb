b6bb8cd42c8481b2aefeef054cbd8449997ceef8
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-10-03 16:24:01 +0100

    Expand default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,20 @@
+if node['roles'].include? 'mysql_primary'
+  server_id = '1'
+elsif node['roles'].include? 'mysql_backup'
+  server_id = '2'
+end
+
+if 'kitchen'.include?(node.chef_environment)
+  secret = Chef::DataBagItem.load('mysql', 'secrets')
+  node.set['mysql-multi']['server_repl_password'] = secret['server_repl_password']
+  node.set['mysql-multi']['slave_user'] = secret['slave_user']
+  Chef::Log.fatal("REPL PASSWORD IS #{secret['server_repl_password']}")
+else
+  secret_file = Chef::EncryptedDataBagItem.load_secret(node['secret_file'])
+  secret = Chef::EncryptedDataBagItem.load('mysql', 'secrets', secret_file)
+  node.set['mysql-multi']['server_repl_password'] = secret['server_repl_password']
+end
+
 user 'mysql'
 
 directory '/etc/mysql' do
@@ -14,8 +31,22 @@ d.group('mysql')
 t = resources(template: '/etc/mysql/conf.d/my.cnf')
 t.owner('mysql')
 t.group('mysql')
+t.variables(serverid: server_id.to_i,
+            cookbook_name: 'mysql-multi',
+            bind_address: node['mysql-multi']['bind_ip'])
 
 firewalld_service 'mysql'
 
-include_recipe 'mysql-multi::mysql-master' if node.roles.include('mysql-primary')
-include_recipe 'mysql-multi::mysql-slave' if node.roles.include('mysql-backup')
+if node.roles.include?('mysql-primary')
+  include_recipe 'mysql-multi::mysql_master'
+  p_t = resources(template: '/etc/mysql/conf.d/master.cnf')
+  p_t.owner('mysql')
+  p_t.group('mysql')
+end
+
+if node.roles.include?('mysql-backup')
+  include_recipe 'mysql-multi::mysql_slave'
+  b_t = resources(template: '/etc/mysql/conf.d/slave.cnf')
+  b_t.owner('mysql')
+  b_t.group('mysql')
+end
