3ccf1b6577bbe72a4f10fead121dfe4f5073feb9
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-10-05 11:45:49 +0100

    Add inspec tests

---
+++ test/integration/primary/mysql.rb
@@ -0,0 +1,16 @@
+describe package('mysql-community-server') do
+ it {should be_installed}
+end
+
+describe service('mysqld') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(3306) do
+  it { should be_listening }
+end
+
+describe file('/etc/mysql/conf.d/my.cnf') do
+  its('content') { should match /server-id = 1/ }
+end
