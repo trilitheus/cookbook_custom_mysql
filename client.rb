6f3a9e2743920f2ccbf2d4a9daf1afe445f68ec6
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-10-05 13:12:22 +0100

    Update client.rb paths

--- client.rb
+++ client.rb
@@ -1,9 +1,9 @@
 # This file is only used when running chef in local mode during image creation
 # cd /home/vagrant/cookbooks/custom_mysql && sudo chef-client -z -c client.rb -o 'recipe[custom_mysql],role[mysql-primary/backup]'
-cookbook_path '/home/vagrant'
-role_path '/home/vagrant/custom_mysql/roles'
-data_bag_path '/home/vagrant/custom_mysql/data_bags'
-environment_path '/home/vagrant/custom_mysql/environments'
+cookbook_path '/home/vagrant/cookbooks'
+role_path '/home/vagrant/cookbooks/custom_mysql/roles'
+data_bag_path '/home/vagrant/cookbooks/custom_mysql/data_bags'
+environment_path '/home/vagrant/cookbooks/custom_mysql/environments'
 environment 'kitchen'
 local_mode 'true'
 log_level :info
