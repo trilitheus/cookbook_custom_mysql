2afa30748e538d96f4d1ca4ef83d7b92844b9517
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-10-05 12:40:34 +0100

    Add client.rb for build automation

---
+++ client.rb
@@ -0,0 +1,9 @@
+# This file is only used when running chef in local mode during image creation
+# cd /home/vagrant/cookbooks/custom_mysql && sudo chef-client -z -c client.rb -o 'recipe[custom_mysql],role[mysql-primary/backup]'
+cookbook_path '/home/vagrant'
+roles_path '/home/vagrant/custom_mysql/roles'
+data_bag_path '/home/vagrant/custom_mysql/data_bags'
+environment_path '/home/vagrant/custom_mysql/environments'
+environment 'kitchen'
+local_mode 'true'
+log_level :info
