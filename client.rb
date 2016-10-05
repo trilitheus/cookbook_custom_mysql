2439b50acb389bf0d257cd582b1421df07168c95
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-10-05 12:54:54 +0100

    Correct role path setting

--- client.rb
+++ client.rb
@@ -1,7 +1,7 @@
 # This file is only used when running chef in local mode during image creation
 # cd /home/vagrant/cookbooks/custom_mysql && sudo chef-client -z -c client.rb -o 'recipe[custom_mysql],role[mysql-primary/backup]'
 cookbook_path '/home/vagrant'
-roles_path '/home/vagrant/custom_mysql/roles'
+role_path '/home/vagrant/custom_mysql/roles'
 data_bag_path '/home/vagrant/custom_mysql/data_bags'
 environment_path '/home/vagrant/custom_mysql/environments'
 environment 'kitchen'
